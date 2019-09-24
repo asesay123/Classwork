b2c58c0c61a5c1288a7ec8af07157a7e5161c010
asesay123 <55443860+asesay123@users.noreply.github.com>
2019-09-24 08:40:52 -0400

    Add files via upload

---
+++ 2019-09-24-Classwork/2019-09-24-Classwork/Source.cpp
@@ -0,0 +1,22 @@
+// Name: Alex Sesay
+// Intro to CS 
+// Mr. Williams
+// Period 1
+// 2019-09-24-Classwork
+
+#include <iostream> 
+
+int main()
+{
+	int x = 1000;
+	int y = 5;
+	float b = 6;
+	char c = 14;
+	bool w = 12;
+	double z = 13;
+	int q = 15;
+	char t = 9;
+	double e = 19;
+	array u = 11;
+	std::cout << sizeof(x) << std::endl;
+}
